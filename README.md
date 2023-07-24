0297d29108ee71f55559a3f0ae1563962f69140e
Matheus Pegorari <69256236+matheuspegorari@users.noreply.github.com>
2023-07-23 23:36:00 -0300

    feat: Developed GET endpoint to retrieve doctor list

--- README.md
+++ README.md
@@ -38,7 +38,12 @@ These instructions will get you a copy of the project up and running on your loc
 
 ## API Endpoints - WIP
 
-- WIP
+Here is a list of the API endpoints and their purposes. Please replace `{base_url}` with your server address (for local development, typically `http://localhost:8080/`).
+- POST `{base_url}/api/v1/medicos` - Creates a new register for doctors.
+- _GET - WIP_
+- _PUT - WIP_
+- _DELETE - WIP_
+
 
 ## Testing
 
@@ -49,10 +54,12 @@ These instructions will get you a copy of the project up and running on your loc
 - [Spring Boot](https://spring.io/projects/spring-boot) - The framework used
 - [Maven](https://maven.apache.org/) - Dependency Management
 - [Java](https://www.java.com/) - The language used
+- [MySQL](https://www.mysql.com/) - The database used
+- [FlywayMigration](https://flywaydb.org/) - For schema version management
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+This project is licensed under the MIT License
 
 ## Contact
 
--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,9 +3,13 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,8 +39,13 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void cadastrarMedico (@RequestBody @Valid DadosCadastroMedico dados){
-        System.out.println(dados);
-
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort={"nome"}) Pageable pageable){
+        return repository
+                .findAll(pageable)
+                .map(DadosListagemMedico::new);
     }
 }

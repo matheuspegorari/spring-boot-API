fea874dafa9ad44ef7b8a5ec0ff31290b1195d9f
Matheus Pegorari <69256236+matheuspegorari@users.noreply.github.com>
2023-07-23 22:25:06 -0300

    feat: Developed POST endpoint for doctor registration

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,19 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
 
     /* Exemplo de RequestBody
     {
@@ -25,7 +33,10 @@ public class MedicoController {
 }
      */
     @PostMapping
-    public void cadastrarMedico (@RequestBody DadosCadastroMedico dados){
+    @Transactional
+    public void cadastrarMedico (@RequestBody @Valid DadosCadastroMedico dados){
         System.out.println(dados);
+
+        repository.save(new Medico(dados));
     }
 }

73df8a67be9282dc7e81d16539ddac5d096f8bc5
Matheus Pegorari <69256236+matheuspegorari@users.noreply.github.com>
2023-07-24 00:10:36 -0300

    feat: Developed PUT endpoint to edit doctor info

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -23,4 +23,14 @@ public class Endereco {
         this.cidade = dados.cidade();
         this.uf = dados.uf();
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (this.logradouro != null) this.logradouro = dados.logradouro();
+        if (this.bairro != null) this.bairro = dados.bairro();
+        if (this.cep != null) this.cep = dados.cep();
+        if (this.numero != null) this.numero = dados.numero();
+        if (this.complemento != null) this.complemento = dados.complemento();
+        if (this.cidade != null) this.cidade = dados.cidade();
+        if (this.uf != null) this.uf = dados.uf();
+    }
 }
